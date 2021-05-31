7d4ec4903d77bf13f8b96957201a24b706b143d8
Kellie <kfullan92@gmail.com>
2021-05-31 17:56:42 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace deliverable2
+{
+    class Program
+    {
+        static void Main(string[] args)
+
+        {
+            bool convo = true;
+            while (convo)
+            {
+                Console.WriteLine("What would you like to say to the bot?");
+                Console.WriteLine("hello");
+                Console.WriteLine("sup");
+                Console.WriteLine("hello there");
+                Console.WriteLine("bye");
+                Console.WriteLine();
+                string input = Console.ReadLine().ToLower();
+          
+                if (input == "hello")
+                {
+                    Console.WriteLine("Hi good to see you" + Environment.NewLine);
+                }
+                else if (input == "sup")
+                {
+                    Console.WriteLine("I am good" + Environment.NewLine);
+                }
+                else if (input == "hello there")
+                {
+                    Console.WriteLine("General Kenobi" + Environment.NewLine);
+                }
+                else if (input == "bye")
+                {
+                    Console.WriteLine("Good bye!" + Environment.NewLine);
+                    convo = false;
+                }
+
+            }
+        }
+    }
+}
